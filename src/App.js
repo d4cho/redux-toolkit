d126243e72c7db83a80349e1928eb8c4b3487a1b
Daniel Cho <danielcho91@gmail.com>
2021-03-05 17:05:37 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titlebar from "./components/Titlebar";
+import Menu from "./components/Menu";
+import { loadRestoAsync, setResto } from "./redux/restoSlice";
+import { useDispatch } from "react-redux";
+import "./App.css";
+
+// dummy data from server
+const name = "Golden Thai";
+const menu = {
+    111: { id: 111, name: "Pad Thai", price: 11.95 },
+    222: { id: 222, name: "Pad See Ew", price: 12.95 },
+    333: { id: 333, name: "Pad Kaprao", price: 13.95 },
+    444: { id: 444, name: "Green Curry", price: 14.95 },
+    555: { id: 555, name: "Massaman Curry", price: 15.95 },
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+
+    // to mimic reaching out to database
+    setTimeout(() => dispatch(setResto({ name, menu })), 1000);
+
+    // dispatch(setResto({ name, menu }));
+    // dispatch(loadRestoAsync());
+    return (
+        <div className="App">
+            <div className="header">
+                <Titlebar />
+            </div>
+            <div className="main">
+                <Menu />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        itemCount: 0,
+        items: {},
+    },
+    reducers: {
+        addItem: (state, action) => {
+            const item = state.items[action.payload.menuId];
+            if (item === undefined) {
+                state.items[action.payload.menuId] = {
+                    menuId: action.payload.menuId,
+                    qty: 1,
+                };
+            } else {
+                item.qty++;
+            }
+            state.itemCount++;
+        },
+        removeItem: (state, action) => {
+            const item = state.items[action.payload.menuId];
+            if (item === undefined) {
+                return;
+            }
+            if (item.qty === 1) {
+                delete state.items[action.payload.menuId];
+            } else {
+                item.qty--;
+            }
+            state.itemCount--;
+        },
+    },
+});
+
+// actions
+export const { addItem, removeItem } = cartSlice.actions;
+
+export const selectCartItems = (state) => state.cart.items;
+export const selectCartCount = (state) => state.cart.itemCount;
+
+export default cartSlice.reducer;
